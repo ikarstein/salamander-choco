8a40f99c3d3a507102efb65ec2854c4a5f4584d7
kenaro <inbox@kenaro.com>
2020-02-10 13:39:58 +0100

    Altap Salamander v4

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+$ErrorActionPreference = 'Stop'; # stop on all errors
+$packageName = 'salamander' # arbitrary name for the package, used in messages
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'ftp://ftp.altap.cz/pub/altap/salamand/as40x86.exe' # download url
+$url64 = 'ftp://ftp.altap.cz/pub/altap/salamand/as40x64.exe' # 64bit URL here or remove - if installer is both, use $url
+$packageArgs = @{
+	packageName = $packageName
+	unzipLocation = $toolsDir
+	fileType = 'EXE' #only one of these: exe, msi, msu
+	url = $url
+	url64bit = $url64
+	silentArgs = '/x /s'
+	validExitCodes = @(0)
+	softwareName = 'salamander*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+	checksum = '3B4E16790DA5D5F979EA9F3D3C7EC5EFAFCAD284A5342F88B89B2A004B6F2FD7AE44899E1CE78D90B9005E345B347E230D875520C681AA5AAA960C12A0097B5D'
+	checksumType = 'sha512' #default is md5, can also be sha1
+	checksum64 = 'D119F06E29DE6CA082A2688588729A69B0D141E6B55268466CCD476684C52F8E7E2E381C5459196C1C4F7B59A188CC92395658D9C7E11B64ECAC73389E6ACBF3'
+	checksumType64= 'sha512' #default is checksumType
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ tools/chocolateyUninstall.ps1
@@ -0,0 +1,19 @@
+try {
+	$packageName = 'salamander'
+	$fileType = "exe"
+	$silentArgs = "/x /s"
+	$validExitCodes = @(0)
+
+	$unfile = "${Env:ProgramFiles}\Altap Salamander\remove\remove.exe"
+	$unfilex86 = "${Env:ProgramFiles(x86)}\Altap Salamander\remove\remove.exe"
+
+	if (Test-Path "$unfilex86") {$file = "$unfilex86"}
+	if (Test-Path "$unfile") {$file = "$unfile"}
+
+	if ((Test-Path "$unfile") -or (Test-Path "$unfilex86")) {
+		Uninstall-ChocolateyPackage -PackageName $packageName  -FileType $fileType -SilentArgs $silentArgs -ValidExitCodes $validExitCodes -File $file
+	}
+} catch {
+	Write-Warning "not able to uninstall Altap Salamander"
+	throw
+}
